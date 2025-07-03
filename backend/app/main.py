c3edb88ed8ca35ff6fdeafc94c2e3fde87a208c9
BriArl <brianna.youte@gmail.com>
2025-07-03 00:55:54 -0400

    updated main.py and registered my routes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
+from app.routes import products
 
 app = FastAPI()
 
-@app.get("/ping")
-async def ping():
-    return {"message": "pong"}
+app.include_router(products.router)
