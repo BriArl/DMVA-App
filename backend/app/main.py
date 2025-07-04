82b8e4160fa9d3f73eb04d9934cac2bf3b64ccbd
BriArl <brianna.youte@gmail.com>
2025-07-04 13:14:39 -0400

    registered moodboard route

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-from app.routes import products, upload
+from app.routes import products, upload, moodboards
 
 app = FastAPI()
 
-app.include_router(products.router)
-app.include_router(upload.router)
+app.include_router(products.router, prefix="/products")
+app.include_router(upload.router, prefix="/upload")
+app.include_router(moodboards.router, prefix="/moodboard")
