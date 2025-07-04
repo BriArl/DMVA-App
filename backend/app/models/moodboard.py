e73ad68a9f0f22d7a08fb686251769bf705e0ef6
BriArl <brianna.youte@gmail.com>
2025-07-04 12:45:05 -0400

    added code to  model

---
+++ backend/app/models/moodboard.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+from typing import List, Optional
+from datetime import datetime
+
+class Moodboard(BaseModel):
+    title: str
+    description: Optional[str] = None
+    image_urls: List[str]
+    created_at: Optional[datetime] = None
