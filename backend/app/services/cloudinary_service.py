3db70ade6e6e708d649ff49674487cd1e0b63e34
BriArl <brianna.youte@gmail.com>
2025-07-04 01:49:44 -0400

    decided to remove the manual approach to connecting cloudinary and created and used the provided API enviromant variable linke from cloudinary and place it in my .env file...now cloudinary reads CLOUDINARY_URL from env. VERY TEA approach :)))

--- backend/app/services/cloudinary_service.py
+++ backend/app/services/cloudinary_service.py
@@ -5,12 +5,6 @@ from dotenv import load_dotenv
 
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), "..", "..", ".env"))
 
-cloudinary.config(
-    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
-    api_key=os.getenv("CLOUDINARY_API_KEY"),
-    api_secret=os.getenv("CLOUDINARY_API_SECERT")
-)
-
 def upload_image(file):
     result = cloudinary.uploader.upload(file)
     return result.get("secure_url")
