ad42efc449079bddd700b0c180cfe31286dfde51
BriArl <brianna.youte@gmail.com>
2025-07-04 01:35:50 -0400

    updated main to include new route for upload

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from app.routes import products
+from app.routes import products, upload
 
 app = FastAPI()
 
 app.include_router(products.router)
+app.include_router(upload.router)
